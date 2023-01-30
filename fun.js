008386651269ef6fc6fd160456aed9a53cc96059
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-01-30 17:39:58 +0530

    initial commit

---
+++ fun.js
@@ -0,0 +1,18 @@
+
+        let body=document.querySelector('body');
+        document.querySelector('#green').onclick =function(){
+            body.style.backgroundColor="green"; 
+        };
+        document.querySelector('#blue').onclick=function() {
+            body.style.backgroundColor='blue';
+        };
+        document.querySelector('#red').onclick=function(){
+            body.style.backgroundColor ='red';
+        };
+        document.querySelector("#yellow").onclick=function() {
+            body.style.backgroundColor ='yellow';
+        };
+        document.querySelector('#pink').onclick=function() {
+            body.style.backgroundColor='pink';
+        };
+
