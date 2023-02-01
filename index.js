aa63e0fdf8d0be170ea1f8aef6d9814c854e1dad
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-01 10:02:39 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-             
+/*             
 document.getElementById("p1").innerHTML="hello";
 
 //getting user input:
@@ -87,5 +87,109 @@ document.getElementById("submit1").onclick = function() {
     document.getElementById("q").innerHTML= q;
 
 }
+*/
+/*
+*/
+/*
+document.getElementById("sub4").onclick= function(){
+    if(documnet.getElementById("pay").checked){
+        window.alert("Thank You for Choosing Paytm!.....");
+    }
+    else if(documnet.getElementById("pho").checked){
+        window.alert("Thank You for Choosing Phonepay!.....");
+    }
+    else if(Gpay.checked){
+        window.alert("Thank You for Choosing Gpay!.....");
+    }
+    else{
+        window.alert("choose the payment method.....");
+    }
+}*/
+/*
+document.getElementById("sub").onclick=function(){
+   
+    //const pay=documnet.getElementById("pay");
+    //const pho=documnet.getElementById("pho");
+    //const Gpay=documnet.getElementById("Gpay");
+
+    if(document.getElementById("check").checked){
+        window.alert("you successfully subscribed!.....");
+    }
+    else{
+        window.alert("yet to subscribe.....");
+    }
+
+    if(documnet.getElementById("pay").checked){
+        window.alert("Thank You for Choosing Paytm!.....");
+    }
+    else if(documnet.getElementById("pho").checked){
+        window.alert("Thank You for Choosing Phonepay!.....");
+    }
+    else if(documnet.getElementById("Gpay").checked){
+        window.alert("Thank You for Choosing Gpay!.....");
+    }
+    else{
+        window.alert("choose the payment method.....");
+    }
+}
+*/
+//if-else
+
+document.getElementById("button").onclick= function() {
+    const pay=document.getElementById("pay");
+    const pho=document.getElementById("pho");
+    const Gpay=document.getElementById("Gpay");
+    
+    
+    if(pay.checked){
+        window.alert("thank you choosing Paytm!...");
+    }
+    else if(pho.checked){
+        window.alert("thank you choosing PhonePay!...");
+    }
+    else if(Gpay.checked){
+        window.alert("thank you choosing GPay!...");
+    }
+    else{
+        window.alert("choose any of the payment option...");
+    }
+}
+
+//nested for loop.
+
+let a=window.prompt("Enter the symble");
+let n=window.prompt("Enter the row");
+let m=window.prompt("Enter the column");
+for(let b=0;b<n;b++){
+    for(let c=0;c<m;c++){
+        document.getElementById("h").innerHTML += a;
+    }
+    document.getElementById("h").innerHTML += "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
