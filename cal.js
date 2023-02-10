14ea59482eaa368246d5386cbe0cba277301c994
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-10 16:57:27 +0530

    initial commit

--- cal.js
+++ cal.js
@@ -1,62 +1,27 @@
-class cal{
+let display = document.getElementById('display');
 
-        constructor(previous,current){
-            this.previous = previous;
-            this.current = current;
+let buttons = Array.from(document.getElementsByClassName('button'));
 
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
         }
-
-        clear(){
-            this.current='';
-            this.previous='';
-            this.operation=undefined;
-
-        }
-
-        delete(){
-
-
-        }
-
-        appendNumber(number){
-            if(number === '.' && this.current.includes('.')) return
-            
-            this.current = this.current.toString() + number.toString(); 
-        }
-
-        operation(operation){
-
-
-        }
-        equal(){
-
-
-        }
-
-        display(){
-            this.current.innerText = this.current;
-
-        }
-
-
-
-}
-
-let number = document.querySelectorAll('[data-number]');
-let add=document.getElementById("plus");
-let sub=document.getElementById("sub");
-let mul=document.getElementById("mul");
-let div=document.getElementById("div");
-let equ=document.getElementById("equ");
-let del=document.getElementById("del");
-let clear=document.getElementById("clearAll");
-let previous = document.getElementById('pre');
-let current = document.getElementById('cur');
-const calulator = new cal(previous,current);
-number.forEach(button =>{
-    button.addEventListener('click',() =>{
-        calulator.appendNumber(button.innerText);
-        calulator.display();
-
-    } );
-})
+    });
+});
