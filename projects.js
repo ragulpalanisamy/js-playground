d46c923d3e2d1276add84203abfab31941f8e48a
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-14 22:35:07 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -37,7 +37,48 @@ document.getElementById('add').onclick = function(){
     document.getElementById('h').innerHTML = count;
 }
 */
+/*
 function ChangeColor(color){
     let container = document.getElementById("container");
     container.style.backgroundColor = color;
+}
+*/
+var weight = window.prompt("enter the weight");
+let height = window.prompt("enter the height");
+let measure = window.prompt("enter the measure");
+let bmi = window.prompt("enter the bmi");
+let error;
+
+function calculate() {
+	//weight = document.getElementById("weight").value;
+	//height = document.getElementById("height").value;
+	error = "Please enter some values";
+	height /= 100;
+	height *= height;
+	bmi = weight/height;
+	bmi = bmi.toFixed(1);
+
+	if (bmi <= 18.4) {
+		measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
+	} else if (bmi >= 18.5 && bmi <= 24.9) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Normal";
+	} else if (bmi >= 25 && bmi <= 29.9) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
+	} else if (bmi >= 30) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
+	}
+	
+
+	if (weight === 0 ) {
+		document.getElementById("results").innerHTML = error;
+	} else if (height === 0){
+		document.getElementById("results").innerHTML = error;
+	}
+	 else {
+
+		document.getElementById("results").innerHTML = measure;
+	}
+	if (weight < 0) {
+		document.getElementById("results").innerHTML = "Negative Values not Allowed";
+	}
 }
