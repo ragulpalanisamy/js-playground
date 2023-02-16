51a859aa32175ca2bc76d1fbd84a287179584d8c
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-16 21:34:43 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -173,7 +173,7 @@ window.onload = function () {
 	}
   }
 */
-
+/*
 let circle = document.getElementById("circle");
 let square = document.getElementById("square");
 let rect = document.getElementById("rect");
@@ -199,8 +199,36 @@ document.getElementById("dis").onclick = function(){
 	square.style.display = "";
 	circle.style.display = "";
 }
+*/
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
 
-
+showTime();
 
 
 
