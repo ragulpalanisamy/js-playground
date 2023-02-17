cb92b6819654aeb56a1a83f17e4215e1d338f420
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-17 21:21:22 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -200,6 +200,7 @@ document.getElementById("dis").onclick = function(){
 	circle.style.display = "";
 }
 */
+/*
 function showTime(){
     var date = new Date();
     var h = date.getHours(); // 0 - 23
@@ -229,7 +230,13 @@ function showTime(){
 }
 
 showTime();
+*/
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+    links.classList.toggle("show-links");
+});
 
 
 
