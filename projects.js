e60186d26e0f5bbc57e94d031049f18121320249
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-20 19:55:16 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -269,6 +269,7 @@ else{
 console.log("Vibration API is not supported in your browser");
 }
 */
+/*
 $(document).ready(function() {
 
 	var magic8Ball = {};
@@ -315,7 +316,67 @@ $(document).ready(function() {
 	$("#questionButton").click(onClick);
   
   });
-  
+*/
+/*
+let a= document.getElementById("US").value;
+let b= document.getElementById("UK").value;
+let content = document.querySelector(".h");
+document.getElementById("click").onclick = function(){
+	if(a == true){
+		document.getElementById("h").innerHTML = "Hi Thanks for Choosing US";
+	}
+	else if(b == true){
+		document.getElementById("h").innerHTML ="Hi Thanks for Choosing UK";
+	}
+	else{
+		window.alert("You didn't choose anything");
+	}
+}  
+
+content.style.visiblity = 'visible';
+*/
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
 
 
 
