f43ffc6cf16da794241b9d90d449654a76d2881a
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-22 19:46:13 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -421,7 +421,8 @@ function counter(){
 		
 }
 */
-
+//stopwatch
+/* 
 const showTime = document.querySelector("#showTime");
 const start = document.querySelector("#start");
 const pause = document.querySelector("#stop");
@@ -484,12 +485,54 @@ function updateTime(){
 	}
 }
 
+ */
 
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelector(".btn");
+let player;
+let computer;
+let result;
 
+choiceBtn.forEach(button => button.addEventListener("click", () => {
+	player = button.textContent;
+	computerTurn();
+	playerText.textContent = `Player: ${player}`;
+	computerText.textContent= `Computer: ${computer}`;
+	resultText.textContent = checkWinner();
+}));
 
+function computerTurn(){
+	const ran = Math.floor(Math.random() * 3) + 1;
+	switch(ran){
+		case 1:
+			computer = "ROCK";
+			break;
+		case 2:
+			computer = "PAPER";
+			break;
+		case 3:
+			computer = "SCISSORS";
+			break;
 
+	}
+}
 
-
+function checkWinner(){
+	if (player == computer){
+		return "Draw!"
+	}
+	else if(computer == "ROCK"){
+		return (player == "PAPER") ? "You Win!" : "You Lose...";
+	}
+	else if(computer == "PAPER"){
+		return (player == "SCISSORS") ? "You Win!" : "You Lose...";
+	}
+	else if(computer == "SCISSORS"){
+		return (player == "ROCK") ? "You Win!" : "You Lose...";
+	}
+}
 
 
 
