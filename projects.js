baeb42cc64544f801a3b1066cc3d1481237c609c
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-03-06 21:58:36 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -589,7 +589,7 @@ function myFunction() {
 
   let  ="Ragul";
 	name.slice();   */
-
+/* 
 	const btn = document.querySelector('button');
 	btn.addEventListener('click', display);
 	function display(){
@@ -658,4 +658,24 @@ function myFunction() {
 	document.getElementsByTagName("option").innerHTML = b[4];
 	document.getElementsByTagName("option").innerHTML = b[5];
 	document.getElementsByTagName("option").innerHTML = b[0]; */
-	
+	 
+
+let btn = document.getElementById("btn");
+let color = document.getElementById("color");
+let wrap = document.getElementById("wrap");
+let back = wrap.getElementsByTagName("div");
+let hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+btn.addEventListener('click',fun);
+function fun(){
+	let col = '#';
+	for(let i=0;i<6;i++){
+		col += random();
+	}
+	color.innerHTML = col;
+	wrap.style.background = col;
+}
+function random(){
+	let random = Math.floor(Math.random(hex) * 16);
+	return hex[random]; 
+}
