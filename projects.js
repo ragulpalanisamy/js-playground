cf2934da16276c825361b248e3fc4300c9a37dc7
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-03-18 15:49:54 +0530

    initial commit

--- projects.js
+++ projects.js
@@ -1013,7 +1013,7 @@ function failure(){
    else{
 	alert("Consonents");
    } */
-
+/* 
    let myPromise = new promise(function(resolve,reject){
 
 	let h=1;
@@ -1030,3 +1030,146 @@ function failure(){
    myPromise.then(value => console.log(value))
    .catch(err => console.log(err));
 
+   let n = document.querySelector("#loader");
+   document.getElementById("x").onclick = function{
+	n.style.display = "none";
+   } */
+/* 
+   let ul = document.getElementById("ul");
+   let f= document.getElementById("add");
+   let input = document.querySelector("input");
+
+   f.addEventListener('click',() =>{
+	let item = input.value;
+	input.value ='';
+	let d = document.createElement('li');
+	let litext = document.createElement('span');
+	let listbtn = document.createElement('button');
+
+	d.appendChild(litext);
+	litext.textContent = item;
+	d.appendChild(listbtn);
+	listbtn.textContent = 'Delete';
+	ul.appendChild(d);
+
+	listbtn.addEventListener('click',()=>{
+		ul.removeChild(d);
+	});
+
+   });
+
+   let parsed = JSON.parse();
+   fetch('https://scrape.abstractapi.com/v1/? api_key = YOUR_API_KEY& url = https://www.apple.com')
+   .then((res)=>res.json())
+   .then((msg)=>msg.setup)
+   .catch((err)=>console.log(err)); */
+   const reviews = [
+	{
+	  id: 1,
+	  name: 'Ragul P',
+	  job: 'web developer',
+	  img: '',
+	  text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+	},
+	{
+	  id: 2,
+	  name: 'Dharu',
+	  job: 'web designer',
+	  img: '',
+	  text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+	},
+	{
+	  id: 3,
+	  name: 'Sri',
+	  job: 'intern',
+	  img: '',
+	  text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+	},
+	{
+	  id: 4,
+	  name: 'Sachin',
+	  job: 'the boss',
+	  img: '',
+	  text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+	},
+  ];
+  // select items
+  const img = document.getElementById('person-img');
+  const author = document.getElementById('author');
+  const job = document.getElementById('job');
+  const info = document.getElementById('info');
+  
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const randomBtn = document.querySelector('.random-btn');
+  
+  // set starting item
+  let currentItem = 0;
+  
+  // load initial item
+  window.addEventListener('DOMContentLoaded', function () {
+	const item = reviews[currentItem];
+	img.src = item.img;
+	author.textContent = item.name;
+	job.textContent = item.job;
+	info.textContent = item.text;
+  });
+  
+  // show person based on item
+  function showPerson(person) {
+	const item = reviews[person];
+	img.src = item.img;
+	author.textContent = item.name;
+	job.textContent = item.job;
+	info.textContent = item.text;
+  }
+  // show next person
+  nextBtn.addEventListener('click', function () {
+	currentItem++;
+	if (currentItem > reviews.length - 1) {
+	  currentItem = 0;
+	}
+	showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener('click', function () {
+	currentItem--;
+	if (currentItem < 0) {
+	  currentItem = reviews.length - 1;
+	}
+	showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener('click', function () {
+	console.log('hello');
+  
+	currentItem = Math.floor(Math.random() * reviews.length);
+	showPerson(currentItem);
+  });
+
+
+      var rows = prompt("How many rows for your multiplication table?");
+    var cols = prompt("How many columns for your multiplication table?");
+    if(rows == "" || rows == null)
+   		 rows = 10;
+    if(cols== "" || cols== null)
+   		 cols = 10;
+    createTable(rows, cols);
+    function createTable(rows, cols)
+    {
+      var j=1;
+      var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
+      for(i=1;i<=rows;i++)
+      {
+    	output = output + "<tr>";
+        while(j<=cols)
+        {
+  		  output = output + "<td>" + i*j + "</td>";
+   		  j = j+1;
+   		}
+   		 output = output + "</tr>";
+   		 j = 1;
+    }
+    output = output + "</table>";
+    document.write(output);
+    }
